ae5c71d620f4397372f60399fdac17364ae52af6
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-24 19:08:32 +0700

    feat(route): register sensor creation endpoint

--- internal/delivery/http/route/route.go
+++ internal/delivery/http/route/route.go
@@ -1,1 +1,24 @@
 package route
+
+import (
+	"iot-subscriber/internal/delivery/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type RouteConfig struct {
+	App              *echo.Echo
+	SensorController *http.SensorController
+}
+
+func (c *RouteConfig) Setup() {
+	c.SetupAuthRoute()
+}
+
+func (c *RouteConfig) SetupAuthRoute() {
+
+	v1 := c.App.Group("/api/v1")
+
+	sensor := v1.Group("/sensor")
+	sensor.POST("/create", c.SensorController.CreateSensor)
+}
