232e2d651405c93430c09205939e4f1bea2f8e30
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-24 19:07:23 +0700

    feat(http): implement SensorController with CreateSensor usecase

---
+++ internal/delivery/http/sensor_controller.go
@@ -0,0 +1,44 @@
+package http
+
+import (
+	"iot-subscriber/internal/model"
+	"iot-subscriber/internal/usecase"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+type SensorController struct {
+	UseCase *usecase.SensorUsecase
+	Log     *logrus.Logger
+}
+
+func NewSensorController(useCase *usecase.SensorUsecase, log *logrus.Logger) *SensorController {
+	return &SensorController{
+		UseCase: useCase,
+		Log:     log,
+	}
+}
+
+func (c SensorController) CreateSensor(ctx echo.Context) error {
+	var request model.CreateSensorRequest
+
+	err := ctx.Bind(&request)
+	if err != nil {
+		c.Log.WithError(err).Error("failed to create sensor record")
+		return err
+	}
+
+	response, err := c.UseCase.Create(ctx.Request().Context(), &request)
+	if err != nil {
+		c.Log.WithError(err).Error("failed to create sensor record")
+		return err
+	}
+
+	return ctx.JSON(http.StatusOK, model.WebResponse[*model.SensorResponse]{Data: response})
+}
+
+func (c SensorController) SearchByCombinedId(ctx echo.Context) error {
+	return nil
+}
