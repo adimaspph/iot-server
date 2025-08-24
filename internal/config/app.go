4c2460b6ed7c707cf15e146e0f893fbeb7484a86
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-24 19:09:20 +0700

    feat(config): wire sensor repositories, usecase, and controller into bootstrap flow

--- internal/config/app.go
+++ internal/config/app.go
@@ -1,6 +1,11 @@
 package config
 
 import (
+	"iot-subscriber/internal/delivery/http"
+	"iot-subscriber/internal/delivery/http/route"
+	"iot-subscriber/internal/repository"
+	"iot-subscriber/internal/usecase"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
@@ -18,5 +23,18 @@ type BootstrapConfig struct {
 
 func Bootstrap(config *BootstrapConfig) {
 	// setup repository
+	sensorRepository := repository.NewSensorRepository(config.Log)
+	sensorRecordRepository := repository.NewSensorRecordRepository(config.Log)
 
+	// setup use cases
+	sensorUseCase := usecase.NewSensorUsecase(config.DB, config.Log, config.Validate, sensorRepository, sensorRecordRepository)
+
+	// setup controller
+	sensorController := http.NewSensorController(sensorUseCase, config.Log)
+
+	routeConfig := route.RouteConfig{
+		App:              config.App,
+		SensorController: sensorController,
+	}
+	routeConfig.Setup()
 }
