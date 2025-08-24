b59ef66798751d134bc14d8aada1ef19e98c0c51
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-24 19:05:31 +0700

    feat(model): add sensor data models for request and response payloads

---
+++ internal/model/sensor_model.go
@@ -0,0 +1,23 @@
+package model
+
+import "time"
+
+type Sensor struct {
+	SensorValue float64   `json:"sensor_value"`
+	Timestamp   time.Time `json:"timestamp"`
+}
+
+type SensorResponse struct {
+	ID1        string   `json:"id1"`
+	ID2        int64    `json:"id2"`
+	SensorType string   `json:"sensor_type"`
+	Sensors    []Sensor `json:"sensors"`
+}
+
+type CreateSensorRequest struct {
+	ID1         string  `json:"id1" validate:"required,uppercase"`
+	ID2         int64   `json:"id2" validate:"required"`
+	SensorType  string  `json:"sensor_type" validate:"required"`
+	SensorValue float64 `json:"sensor_value" validate:"required"`
+	Timestamp   string  `json:"timestamp" validate:"required,datetime=2006-01-02T15:04:05.000Z"`
+}
