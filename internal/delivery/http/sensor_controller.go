f8091d59c1c590506fa48d4c9f8f986a80593f36
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-26 03:35:56 +0700

    feat(http): add DeleteByCombinedId controller

--- internal/delivery/http/sensor_controller.go
+++ internal/delivery/http/sensor_controller.go
@@ -125,3 +125,23 @@ func (c SensorController) SearchByIdAndTimeRange(ctx echo.Context) error {
 		Paging: metadata,
 	})
 }
+
+func (c SensorController) DeleteByCombinedId(ctx echo.Context) error {
+	var request model.SensorSearchByIdRequest
+
+	err := ctx.Bind(&request)
+	if err != nil {
+		c.Log.WithError(err).Error("failed to bind request")
+		return err
+	}
+
+	response, err := c.UseCase.DeleteByIdCombination(ctx.Request().Context(), &request)
+	if err != nil {
+		c.Log.WithError(err).Error("failed to search sensor record")
+		return err
+	}
+
+	return ctx.JSON(http.StatusOK, model.WebResponse[*model.SensorDeleteResponse]{
+		Data: response,
+	})
+}
