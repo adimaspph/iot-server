2f50b10202ba6f411d7f4df9d1aaa8f0acb84f03
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-28 02:41:05 +0700

    feat(model): add Role field to UserResponse

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -4,6 +4,7 @@ type UserResponse struct {
 	ID        string `json:"id,omitempty"`
 	Name      string `json:"name,omitempty"`
 	Token     string `json:"token,omitempty"`
+	Role      string `json:"role,omitempty"`
 	CreatedAt int64  `json:"created_at,omitempty"`
 	UpdatedAt int64  `json:"updated_at,omitempty"`
 }
