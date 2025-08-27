958402321e55b766a0e017410b4e26168c691051
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-28 03:52:43 +0700

    feat(model): add Role field to RegisterUserRequest

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -17,6 +17,7 @@ type RegisterUserRequest struct {
 	ID       string `json:"id" validate:"required,max=100"`
 	Password string `json:"password" validate:"required,max=100"`
 	Name     string `json:"name" validate:"required,max=100"`
+	Role     string `json:"role" validate:"required,max=100"`
 }
 
 type UpdateUserRequest struct {
