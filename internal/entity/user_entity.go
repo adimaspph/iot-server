90de705566d64a5cee53f959072fbe0819cfcc56
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-28 01:59:26 +0700

    feat(entity): add Role field and UserRole constants to User struct

--- internal/entity/user_entity.go
+++ internal/entity/user_entity.go
@@ -5,6 +5,14 @@ type User struct {
 	ID        string
 	Password  string
 	Name      string
+	Role      string
 	CreatedAt int64
 	UpdatedAt int64
 }
+
+type UserRole string
+
+const (
+	RoleAdmin UserRole = "admin"
+	RoleUser  UserRole = "user"
+)
