d075140e27562e98013c72948c7d8045f7436c47
Adimas Putra Pratama H <adimaspph@gmail.com>
2025-08-27 19:13:17 +0700

    feat(usecase): implement create user

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -5,13 +5,16 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"iot-server/internal/entity"
 	"iot-server/internal/model"
 	"iot-server/internal/repository"
 	"iot-server/internal/util"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserUsecase struct {
@@ -55,4 +58,63 @@ func (u *UserUsecase) Verify(ctx context.Context, request *model.VerifyUserReque
 	}
 
 	return &model.Auth{ID: user.ID}, nil
+}
+
+func (u *UserUsecase) Create(ctx context.Context, request *model.RegisterUserRequest) (*model.UserResponse, error) {
+	if err := u.Validate.Struct(request); err != nil {
+		u.Log.WithError(err).Error("failed to validate request")
+		return nil, echo.ErrBadRequest
+	}
+
+	// Check existence
+	if _, err := u.Repository.FindByID(ctx, request.ID); err == nil {
+		u.Log.Warn("user id already exists")
+		return nil, echo.ErrConflict
+	} else if !errors.Is(err, sql.ErrNoRows) {
+		// user not found
+		u.Log.WithError(err).Error("failed to find user by id")
+		return nil, echo.ErrInternalServerError
+	}
+
+	// Hash password
+	hash, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+	if err != nil {
+		u.Log.WithError(err).Error("failed to hashing password")
+		return nil, echo.ErrInternalServerError
+	}
+
+	user := &entity.User{
+		ID:        request.ID,
+		Name:      request.Name,
+		Password:  string(hash),
+		Token:     "",
+		CreatedAt: time.Now().UnixMilli(),
+		UpdatedAt: time.Now().UnixMilli(),
+	}
+
+	// begin tx
+	tx, err := u.DB.BeginTx(ctx, &sql.TxOptions{})
+	if err != nil {
+		u.Log.WithError(err).Error("failed to begin transaction")
+		return nil, echo.ErrInternalServerError
+	}
+	defer func() {
+		_ = tx.Rollback()
+	}()
+
+	if err := u.Repository.Create(ctx, user); err != nil {
+		u.Log.WithError(err).Error("failed to create user")
+		return nil, echo.ErrInternalServerError
+	}
+
+	// commit
+	if err := tx.Commit(); err != nil {
+		u.Log.WithError(err).Error("failed to commit transaction")
+		return nil, echo.ErrInternalServerError
+	}
+
+	return &model.UserResponse{
+		ID:   user.ID,
+		Name: user.Name,
+	}, nil
 }
